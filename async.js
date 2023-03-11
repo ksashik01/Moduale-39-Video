8aeb159f8eace8acf475339ed534b228a9a1d11e
ksashik01 <kscomplainsheet@gmail.com>
2023-03-12 01:07:13 +0600

    first commit

---
+++ async.js
@@ -0,0 +1,19 @@
+console.log (1);
+console.log (2);
+
+setTimeout (()=>{
+console.log ('lazy Logged')
+
+
+}, 4000)
+
+console.log (4);
+console.log (5);
+console.log (6);
+
+function doSomething (){
+    console.log (3);
+
+
+
+}
---
+++ promise.js
@@ -0,0 +1,50 @@
+
+
+// const getData = new Promise ((resolve,reject) =>{
+
+//     resolve (566665);
+
+
+
+// }  )
+
+// // console.log (getData)
+
+// getData.then (data => console.log (data));
+
+
+
+
+
+const getData = new Promise ((resolve,reject) =>{
+
+    // reject ('no data available');
+
+    const num = Math.random ()*10;
+    console.log (num);
+    if (num < 5){
+
+        resolve (566665)
+
+    }
+    else{
+            reject ('no data available');
+
+    }
+
+
+
+}  )
+
+// console.log (getData)
+
+getData
+.then (data => console.log (data))
+.catch (err => console.log ('ERR', err) )
+
+
+
+
+
+
+
