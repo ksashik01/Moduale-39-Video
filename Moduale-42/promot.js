5e6d2812fa886a19169df5609c75d0d18d4c1b33
ksashik01 <kscomplainsheet@gmail.com>
2023-03-17 00:05:52 +0600

    file

---
+++ Moduale-42/promot.js
@@ -0,0 +1,40 @@
+
+
+const showAler = ()=>{
+
+alert ('danger area go away')
+
+}
+
+
+const landMoney = ()=>{
+// confirm ('i think');
+
+const result = confirm ('i think');
+console.log (result);
+if (result===true){
+
+    alert ('tui amar dosto');
+}
+else {
+console.log ('chini na ')
+
+}
+
+
+}
+
+const clickMe = ()=>{
+
+const name = prompt ('tell me your name')
+console.log (name)
+
+if (name === null){
+    alert ('name na dile kamna ')
+}
+else{
+    console.log ('welcome this world')
+}
+
+
+}
